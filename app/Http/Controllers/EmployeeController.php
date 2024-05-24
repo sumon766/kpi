cfc4e371365771eb9dfc45f2fd0044dea225c624
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-05-24 21:21:40 +0600

    Sending employee over api and creating as well

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $employees = Employee::all();
+        return response()->json($employees);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'designation' => 'required',
+            'sector_id' => 'required'
+        ]);
+
+        $employee = Employee::create($validatedData);
+
+        return response()->json($employee, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SectorController;
+use App\Http\Controllers\EmployeeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('sectors', SectorController::class);
+Route::apiResource('employees', EmployeeController::class);
