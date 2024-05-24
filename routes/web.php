37f876d909793f4dda29fb9a92fcda7cc5e5d2eb
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-05-24 18:27:50 +0600

    Sending sectors data to frontend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SectorController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('api')->group(function () {
+    Route::apiResource('sectors', SectorController::class);
 });
