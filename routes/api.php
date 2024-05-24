9cbdbdc82889518210a5db6c74786ee91689911c
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-05-24 23:46:33 +0600

    Setup question model migration and controller

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SectorController;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\QuestionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,4 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('sectors', SectorController::class);
 Route::apiResource('employees', EmployeeController::class);
+Route::apiResource('questions', QuestionController::class);
